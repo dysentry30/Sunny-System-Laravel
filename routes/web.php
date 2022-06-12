81107841087ab3f2ede93aea235fc5b54a2884b9
dysentry30 <bagassatrianurwinanto@gmail.com>
2022-06-12 14:53:53 +0700

    Membuat halaman Document bisa melihat document di web

--- routes/web.php
+++ routes/web.php
@@ -52,201 +52,215 @@ use App\Http\Controllers\PasalController;
 
 
 
-Route::get('/', function () { return view('1_Dashboard'); });
+Route::get('/', function () {
+    return view('1_Dashboard');
+});
 
 
 // begin :: contract management
-    Route::get('/contract-management', [ContractManagementsController::class, 'index']);
+Route::get('/contract-management', [ContractManagementsController::class, 'index']);
 
-    Route::get('/contract-management/view', [ContractManagementsController::class, 'new']);
+Route::get('/contract-management/view', [ContractManagementsController::class, 'new']);
 
-    Route::post('/contract-management/save', [ContractManagementsController::class, 'save']);
+Route::post('/contract-management/save', [ContractManagementsController::class, 'save']);
 
-    Route::post('/contract-management/update', [ContractManagementsController::class, 'update']);
+Route::post('/contract-management/update', [ContractManagementsController::class, 'update']);
 
-    Route::get('/contract-management/view/{id_contract}', [ContractManagementsController::class, 'viewContract']);
+Route::get('/contract-management/view/{id_contract}', [ContractManagementsController::class, 'viewContract']);
 
-    Route::get('/contract-management/view/{id_contract}/addendum-contract', [ContractManagementsController::class, 'addendumContract']);
-    
-    Route::get('/contract-management/view/{id_contract}/addendum-contract/{addendumContract}', [ContractManagementsController::class, 'addendumView']);
-    
-    Route::get('/contract-management/view/{id_contract}/addendum-contract/{addendumContract}/new', [ContractManagementsController::class, 'addendumNew']);
-    
-    Route::get('/contract-management/view/{id_contract}/addendum-contract/{addendumContract}/{addendumDraft}', [ContractManagementsController::class, 'addendumDraft']);
-    
-    Route::get('/contract-management/view/{id_contract}/draft-contract', [ContractManagementsController::class, 'draftContract']);
-    
-    Route::get('/contract-management/view/{id_contract}/draft-contract/{draftContracts}', [ContractManagementsController::class, 'draftContractView']);
-    
-    Route::get('/contract-management/view/{id_contract}/draft-contract/{is_tender_menang}', [ContractManagementsController::class, 'tenderMenang']);
-    
+Route::get('/contract-management/view/{id_contract}/addendum-contract', [ContractManagementsController::class, 'addendumContract']);
+
+Route::get('/contract-management/view/{id_contract}/addendum-contract/{addendumContract}', [ContractManagementsController::class, 'addendumView']);
+
+Route::get('/contract-management/view/{id_contract}/addendum-contract/{addendumContract}/new', [ContractManagementsController::class, 'addendumNew']);
+
+Route::get('/contract-management/view/{id_contract}/addendum-contract/{addendumContract}/{addendumDraft}', [ContractManagementsController::class, 'addendumDraft']);
+
+Route::get('/contract-management/view/{id_contract}/draft-contract', [ContractManagementsController::class, 'draftContract']);
+
+Route::get('/contract-management/view/{id_contract}/draft-contract/{draftContracts}', [ContractManagementsController::class, 'draftContractView']);
+
+Route::get('/contract-management/view/{id_contract}/draft-contract/{is_tender_menang}', [ContractManagementsController::class, 'tenderMenang']);
+
 // end :: contract management
 
 
 
 // begin :: Pasal
-    Route::get('/pasal/edit', [PasalController::class, 'index']);
+Route::get('/pasal/edit', [PasalController::class, 'index']);
 
-    Route::get('/pasal/delete/{pasal}', [PasalController::class, 'destroy']);
+Route::get('/pasal/delete/{pasal}', [PasalController::class, 'destroy']);
 
-    Route::get('/pasal/{pasal}', [PasalController::class, 'show']);
+Route::get('/pasal/{pasal}', [PasalController::class, 'show']);
 
-    Route::get('change-request', [PasalController::class, 'changeRequest']);
-    
+Route::get('change-request', [PasalController::class, 'changeRequest']);
+
 // end :: Pasal
 
 
 // begin :: Claim Management
-    Route::get('claim-management', [ClaimController::class, 'index']);
+Route::get('claim-management', [ClaimController::class, 'index']);
 
-    Route::get('claim-management/{proyek}/{contract}/new',  [ClaimController::class, 'new']);
+Route::get('claim-management/{proyek}/{contract}/new',  [ClaimController::class, 'new']);
 
-    Route::post('/claim-management/save', [ClaimController::class, 'save']);
+Route::post('/claim-management/save', [ClaimController::class, 'save']);
 
-    Route::get('claim-management/view/{claim_management}', [ClaimController::class, 'show']);
+Route::get('claim-management/view/{claim_management}', [ClaimController::class, 'show']);
 
-    Route::post('/approval-claim/save', [ClaimController::class, 'store']);
+Route::post('/approval-claim/save', [ClaimController::class, 'store']);
 
-    Route::post('/approval-claim/delete', [ClaimController::class, 'delete']);
+Route::post('/approval-claim/delete', [ClaimController::class, 'delete']);
 
-    Route::post('/claim-management/update', [ClaimController::class, 'update']);
+Route::post('/claim-management/update', [ClaimController::class, 'update']);
 
-    Route::post('/detail-claim/save', [ClaimController::class, 'detailSave']);
+Route::post('/detail-claim/save', [ClaimController::class, 'detailSave']);
 
-    Route::post('/claim/stage/save', [ClaimController::class, 'claimStage']);
+Route::post('/claim/stage/save', [ClaimController::class, 'claimStage']);
 
 // end :: Claim Management
 
 // Begin :: Menu Document
-    Route::get('/document', function () {
-        $all_document = [];
-        $tables = DB::select("SELECT table_name
+Route::get('/document', function () {
+    $all_document = [];
+    $tables = DB::select("SELECT table_name
                         FROM information_schema.columns
                         WHERE column_name='id_document';");
-            foreach ($tables as $table) {
-                $table_name = $table->table_name;
-                $data = DB::select("SELECT * FROM $table_name;");
-                if (!empty($data)) {
-                    array_push($all_document, $data);
-                }
+    foreach ($tables as $table) {
+        $table_name = $table->table_name;
+        $data = DB::select("SELECT * FROM $table_name;");
+        if (!empty($data)) {
+            array_push($all_document, $data);
+        }
+    }
+    $all_document = array_merge(...$all_document);
+    $id_documents = array_map(function ($array) {
+        return array_values((array) $array);
+    }, $all_document);
+    $documents_name = array_map(function ($array) {
+        $array = get_object_vars($array);
+        $array_keys = array_keys($array);
+        foreach ($array_keys as $key) {
+            if (str_contains($key, "document_name")) {
+                return $array[$key];
             }
-        $all_document = array_merge(...$all_document);
-        return view("document/view", ["all_document" => $all_document]);
-    });
+        }
+    }, $all_document);
+    return view("document/view", ["all_document" => $all_document, "id_documents" => $id_documents, "documents_name" => $documents_name]);
+});
 // End :: Menu Document
 
 
 //Begin :: Customer
-    // customer dashboard all database
-    Route::get('/customer', [CustomerController::class, 'index']);
+// customer dashboard all database
+Route::get('/customer', [CustomerController::class, 'index']);
 
 
-    // DELETE data customer pada dasboard customer by ID 
-    Route::delete('customer/delete/{id_customer}', [CustomerController::class, 'delete']);
+// DELETE data customer pada dasboard customer by ID 
+Route::delete('customer/delete/{id_customer}', [CustomerController::class, 'delete']);
 
 
-    // view customer by id_customer #1
-    Route::get('/customer/view/{id_customer}', [CustomerController::class, 'view']);
+// view customer by id_customer #1
+Route::get('/customer/view/{id_customer}', [CustomerController::class, 'view']);
 
 
 
-    // EDIT customer by view id_customer #2   
-    Route::post('/customer/save-edit', [CustomerController::class, 'saveEdit']);
+// EDIT customer by view id_customer #2   
+Route::post('/customer/save-edit', [CustomerController::class, 'saveEdit']);
 
 
-    // NEW to Create New customer #1 
-    Route::get('/customer/new', [CustomerController::class, 'new']);
+// NEW to Create New customer #1 
+Route::get('/customer/new', [CustomerController::class, 'new']);
 
 
-    // NEW to Create New customer #2
-    Route::post('/customer/save', [CustomerController::class, 'saveNew']);
- 
+// NEW to Create New customer #2
+Route::post('/customer/save', [CustomerController::class, 'saveNew']);
 
-    // Edit Customer Proyek History by view id_customer    
-    Route::post('/customer/save-modal', [CustomerController::class, 'customerHistory']);
+
+// Edit Customer Proyek History by view id_customer    
+Route::post('/customer/save-modal', [CustomerController::class, 'customerHistory']);
 //End :: Customer
 
 
 
 //Begin :: Project
 
-    // Home Page Proyek
-    Route::get('/project', [ProyekController::class, 'view']);
+// Home Page Proyek
+Route::get('/project', [ProyekController::class, 'view']);
 
-    // to NEW page 
-    Route::get('/proyek/new', [ProyekController::class, 'new']);
+// to NEW page 
+Route::get('/proyek/new', [ProyekController::class, 'new']);
 
-    // direct to proyek after SAVE page 
-    Route::post('/proyek/save', [ProyekController::class, 'save']);
+// direct to proyek after SAVE page 
+Route::post('/proyek/save', [ProyekController::class, 'save']);
 
-    // VIEW to proyek and EDIT 
-    Route::get('/proyek/view/{id}', [ProyekController::class, 'edit']);
+// VIEW to proyek and EDIT 
+Route::get('/proyek/view/{id}', [ProyekController::class, 'edit']);
 
-    // direct to Project after EDIT 
-    Route::post('/proyek/update', [ProyekController::class, 'update']);
+// direct to Project after EDIT 
+Route::post('/proyek/update', [ProyekController::class, 'update']);
 
-    // DELETE data customer pada dasboard customer by ID 
-    Route::delete('proyek/delete/{kode_proyek}', [ProyekController::class, 'delete']);
-    
-    // Stage Update 
-    Route::post('/proyek/stage-save', function (Request $request) {
-        $id = $request->id;
-        $proyekStage = Proyek::find($id);
-        $proyekStage->stage = $request->stage;
-        // dd($proyekStage);
-        if ($proyekStage->save()) {
-            return response()->json([
-                "status" => "success",
-                "link" => true,
-            ]);
-        }
-    });
+// DELETE data customer pada dasboard customer by ID 
+Route::delete('proyek/delete/{kode_proyek}', [ProyekController::class, 'delete']);
+
+// Stage Update 
+Route::post('/proyek/stage-save', function (Request $request) {
+    $id = $request->id;
+    $proyekStage = Proyek::find($id);
+    $proyekStage->stage = $request->stage;
+    // dd($proyekStage);
+    if ($proyekStage->save()) {
+        return response()->json([
+            "status" => "success",
+            "link" => true,
+        ]);
+    }
+});
 
 //End :: Project
 
 
 //Begin :: Forecast
 
-    // Home Page Forecast
-    Route::get('/forecast', [ForecastController::class, 'index']);
+// Home Page Forecast
+Route::get('/forecast', [ForecastController::class, 'index']);
 
-    // to NEW page 
-    // Route::get('/proyek/new', [ProyekController::class, 'new']);
+// to NEW page 
+// Route::get('/proyek/new', [ProyekController::class, 'new']);
 
 //End :: Forecast
 
 
 // Begin :: Master Data
 
-    // Home Page Company
-    Route::get('/company', [CompanyController::class, 'index']);
+// Home Page Company
+Route::get('/company', [CompanyController::class, 'index']);
 
-    // NEW Company after SAVE 
-    Route::post('/company/save', [CompanyController::class, 'store']);
-    
-    // Home Sumber Dana
-    Route::get('/sumber-dana', [SumberDanaController::class, 'index']);
-    
-    // NEW Sumber Dana after SAVE
-    Route::post('/sumber-dana/save', [SumberDanaController::class, 'store']);
+// NEW Company after SAVE 
+Route::post('/company/save', [CompanyController::class, 'store']);
 
-    // Home DOP
-    Route::get('/dop', [DopController::class, 'index']);
+// Home Sumber Dana
+Route::get('/sumber-dana', [SumberDanaController::class, 'index']);
 
-    // NEW DOP after SAVE
-    Route::post('/dop/save', [DopController::class, 'store']);
+// NEW Sumber Dana after SAVE
+Route::post('/sumber-dana/save', [SumberDanaController::class, 'store']);
 
-    // Home SBU
-    Route::get('/sbu', [SbuController::class, 'index']);
+// Home DOP
+Route::get('/dop', [DopController::class, 'index']);
 
-    // NEW SBU after SAVE
-    Route::post('/sbu/save', [SbuController::class, 'store']);
+// NEW DOP after SAVE
+Route::post('/dop/save', [DopController::class, 'store']);
 
-    // Home Unit Kerja
-    Route::get('/unit-kerja', [UnitKerjaController::class, 'index']);
+// Home SBU
+Route::get('/sbu', [SbuController::class, 'index']);
 
-    // NEW Unit Kerja after SAVE
-    Route::post('/unit-kerja/save', [UnitKerjaController::class, 'store']);
+// NEW SBU after SAVE
+Route::post('/sbu/save', [SbuController::class, 'store']);
+
+// Home Unit Kerja
+Route::get('/unit-kerja', [UnitKerjaController::class, 'index']);
+
+// NEW Unit Kerja after SAVE
+Route::post('/unit-kerja/save', [UnitKerjaController::class, 'store']);
 
 //End :: Master Data
 
@@ -280,7 +294,7 @@ Route::get('/', function () { return view('1_Dashboard'); });
 function moveFileTemp(UploadedFile $file, $file_name)
 {
     $result = $file->storeAs("public/words", $file_name . "." . $file->getClientOriginalExtension());
-    
+
     return $result;
 }
 
