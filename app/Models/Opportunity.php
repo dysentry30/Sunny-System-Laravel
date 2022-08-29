f209715ac930a0bcb567261a64c2466103cec018
dysentry30 <bagassatrianurwinanto@gmail.com>
2022-08-29 17:18:43 +0700

    Update: - New Opportunity for Present - Model Opportunity - Claim Controller Fixed New Claim

---
+++ app/Models/Opportunity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Opportunity extends Model
+{
+    use HasFactory;
+    protected $table = "opportunity";
+    protected $casts = [
+        "Id" => "string",
+    ];
+}
