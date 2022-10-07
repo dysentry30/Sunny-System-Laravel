8a58f3479a0d730a3fd248fe7c1c2dc77328a4f9
rahmadar-if <rahmadarifwork@gmail.com>
2022-10-07 09:40:21 +0700

    Update: - Struktur Attachment di Customer 360

---
+++ app/Models/StrukturAttachment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StrukturAttachment extends Model
+{
+    use HasFactory;
+    protected $primaryKey   = 'id_struktur_attachment';
+}
---
+++ database/factories/StrukturAttachmentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\StrukturAttachment>
+ */
+class StrukturAttachmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
