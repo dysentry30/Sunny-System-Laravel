98e5032cd24bc6578b7b9bd7b5b567500cb6e1bc
dysentry30 <bagassatrianurwinanto@gmail.com>
2022-11-09 10:28:53 +0700

    unset tipe proyek API nilai proyek

--- routes/api.php
+++ routes/api.php
@@ -228,7 +228,7 @@ Route::middleware(["web"])->group(function () {
                 $p->component_id = 0;
                 $p->header_id = 0;
                 $p->data_ok = $data_ok;
-                unset($p->kode_proyek, $p->nama_proyek, $p->jenis_proyek, $p->unit_kerja, $p->nilai_perolehan, $p->is_cancel, $p->stage);
+                unset($p->kode_proyek, $p->nama_proyek, $p->jenis_proyek, $p->unit_kerja, $p->nilai_perolehan, $p->is_cancel, $p->stage, $p->tipe_proyek);
                 // $p->nilai_forecast = $p->forecasts->sum("nilai_forecast");
                 // $p->rkap_forecast = $p->forecasts->sum("rkap_forecast");
                 // $p->realisasi_forecast = $p->forecasts->sum("realisasi_forecast");
