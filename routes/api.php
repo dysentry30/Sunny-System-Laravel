be9c6734462a476545645ef8dffc84a51ef6fdd7
dysentry30 <bagassatrianurwinanto@gmail.com>
2022-11-10 10:25:54 +0700

    ambigus

--- routes/api.php
+++ routes/api.php
@@ -46,102 +46,102 @@ Route::middleware(["web"])->group(function () {
     // End Login / Logout
 
     // Begin Detail Proyek yang ada forecast
-    Route::post('/detail-proyek', function (Request $request) {
-        $periode = getPeriode($request->periode);
-        $is_bpmcsrf_exist = $request->header("BPMCSRF");
-        if (isset($is_bpmcsrf_exist)) {
+    // Route::post('/detail-proyek', function (Request $request) {
+    //     $periode = getPeriode($request->periode);
+    //     $is_bpmcsrf_exist = $request->header("BPMCSRF");
+    //     if (isset($is_bpmcsrf_exist)) {
 
-            // $forecasts = Forecast::with(["Proyek"])->get(["*"])->unique("kode_proyek");
-            // $forecasts = Proyek::where("periode_prognosa", '=', (int) $prognosa)->whereYear("created_at", "=", $tahun)->get();
-            if (isset($request->unitkerjaid)) {
-                // $proyeks = Proyek::where("unit_kerja", "=", $request->unitkerjaid)->where("tahun_perolehan", "=", $periode[0])->where("bulan_pelaksanaan", "=", $periode[1])->get(["nama_proyek", "kode_proyek", "unit_kerja", "jenis_proyek", "stage", "tanggal_mulai_terkontrak", "tanggal_akhir_terkontrak"]);
-                $proyeks = Proyek::where("unit_kerja", "=", $request->unitkerjaid)->where("tahun_perolehan", "=", $periode[0])->get(["nama_proyek", "kode_proyek", "unit_kerja", "jenis_proyek", "stage", "tanggal_mulai_terkontrak", "tanggal_akhir_terkontrak"])->filter(function ($p) use ($periode) {
-                    $is_forecast_exist = $p->Forecasts->where("periode_prognosa", $periode[1])->count() > 0;
-                    unset($p->Forecasts);
-                    return $is_forecast_exist;
-                });
-            } else {
-                return response()->json([
-                    "status" => 400,
-                    "msg" => "Unit Kerja Not Found"
-                ], 400);
-            }
-            $proyeks = $proyeks->map(function ($p) use ($request) {
-                if (str_contains($p->kode_proyek, "KD")) {
-                    $p->kode_crm = Illuminate\Support\Facades\DB::table('proyek_code_crm')->where("kode_proyek", "=", $p->kode_proyek)->first()->kode_proyek_crm ?? $p->kode_proyek;
-                } else {
-                    $p->kode_crm = $p->kode_proyek;
-                }
-                $p->nama_proyek = $p->nama_proyek;
-                $p->departemen_id = $p->unit_kerja;
-                $p->ap_id = "";
-                switch ($p->jenis_proyek) {
-                    case "I":
-                        $p->jenis = "INTERN";
-                        break;
-                    case "N":
-                        $p->jenis = "EXTERN";
-                        break;
-                    case "J":
-                        $p->jenis = "JO";
-                        break;
-                }
-                $p->kategori = "PROYEK";
+    //         // $forecasts = Forecast::with(["Proyek"])->get(["*"])->unique("kode_proyek");
+    //         // $forecasts = Proyek::where("periode_prognosa", '=', (int) $prognosa)->whereYear("created_at", "=", $tahun)->get();
+    //         if (isset($request->unitkerjaid)) {
+    //             // $proyeks = Proyek::where("unit_kerja", "=", $request->unitkerjaid)->where("tahun_perolehan", "=", $periode[0])->where("bulan_pelaksanaan", "=", $periode[1])->get(["nama_proyek", "kode_proyek", "unit_kerja", "jenis_proyek", "stage", "tanggal_mulai_terkontrak", "tanggal_akhir_terkontrak"]);
+    //             $proyeks = Proyek::where("unit_kerja", "=", $request->unitkerjaid)->where("tahun_perolehan", "=", $periode[0])->get(["nama_proyek", "kode_proyek", "unit_kerja", "jenis_proyek", "stage", "tanggal_mulai_terkontrak", "tanggal_akhir_terkontrak"])->filter(function ($p) use ($periode) {
+    //                 $is_forecast_exist = $p->Forecasts->where("periode_prognosa", $periode[1])->count() > 0;
+    //                 unset($p->Forecasts);
+    //                 return $is_forecast_exist;
+    //             });
+    //         } else {
+    //             return response()->json([
+    //                 "status" => 400,
+    //                 "msg" => "Unit Kerja Not Found"
+    //             ], 400);
+    //         }
+    //         $proyeks = $proyeks->map(function ($p) use ($request) {
+    //             if (str_contains($p->kode_proyek, "KD")) {
+    //                 $p->kode_crm = Illuminate\Support\Facades\DB::table('proyek_code_crm')->where("kode_proyek", "=", $p->kode_proyek)->first()->kode_proyek_crm ?? $p->kode_proyek;
+    //             } else {
+    //                 $p->kode_crm = $p->kode_proyek;
+    //             }
+    //             $p->nama_proyek = $p->nama_proyek;
+    //             $p->departemen_id = $p->unit_kerja;
+    //             $p->ap_id = "";
+    //             switch ($p->jenis_proyek) {
+    //                 case "I":
+    //                     $p->jenis = "INTERN";
+    //                     break;
+    //                 case "N":
+    //                     $p->jenis = "EXTERN";
+    //                     break;
+    //                 case "J":
+    //                     $p->jenis = "JO";
+    //                     break;
+    //             }
+    //             $p->kategori = "PROYEK";
 
-                switch ($p->stage) {
-                    case 0:
-                        $p->tahap = "Pasar Dini";
-                        break;
-                    case 1:
-                        $p->tahap = "Pasar Dini";
-                        break;
-                    case 2:
-                        $p->tahap = "Pasar Potensial";
-                        break;
-                    case 3:
-                        $p->tahap = "Prakualifikasi";
-                        break;
-                    case 4:
-                        $p->tahap = "Tender Diikuti";
-                        break;
-                    case 5:
-                        $p->tahap = "Perolehan";
-                        break;
-                    case 6:
-                        $p->tahap = "Menang";
-                        break;
-                    case 7:
-                        $p->tahap = "Terendah";
-                        break;
-                    case 8:
-                        $p->tahap = "Terkontrak";
-                        break;
-                }
-                // dd($p->tanggal_mulai_terkontrak, $p->tanggal_akhir_terkontrak);
-                $p->pemberi_kerja = ProyekBerjalans::where("kode_proyek", "=", $p->kode_proyek)->first()->name_customer ?? "";
-                $p->rencana_perolehan = null;
-                $p->perkiraan_durasi = date_create($p->tanggal_mulai_terkontrak)->diff(date_create($p->tanggal_akhir_terkontrak))->days;
-                $p->periode = $request->periode;
-                unset($p->jenis_proyek, $p->unit_kerja, $p->kode_proyek, $p->stage, $p->tanggal_mulai_terkontrak, $p->tanggal_akhir_terkontrak);
+    //             switch ($p->stage) {
+    //                 case 0:
+    //                     $p->tahap = "Pasar Dini";
+    //                     break;
+    //                 case 1:
+    //                     $p->tahap = "Pasar Dini";
+    //                     break;
+    //                 case 2:
+    //                     $p->tahap = "Pasar Potensial";
+    //                     break;
+    //                 case 3:
+    //                     $p->tahap = "Prakualifikasi";
+    //                     break;
+    //                 case 4:
+    //                     $p->tahap = "Tender Diikuti";
+    //                     break;
+    //                 case 5:
+    //                     $p->tahap = "Perolehan";
+    //                     break;
+    //                 case 6:
+    //                     $p->tahap = "Menang";
+    //                     break;
+    //                 case 7:
+    //                     $p->tahap = "Terendah";
+    //                     break;
+    //                 case 8:
+    //                     $p->tahap = "Terkontrak";
+    //                     break;
+    //             }
+    //             // dd($p->tanggal_mulai_terkontrak, $p->tanggal_akhir_terkontrak);
+    //             $p->pemberi_kerja = ProyekBerjalans::where("kode_proyek", "=", $p->kode_proyek)->first()->name_customer ?? "";
+    //             $p->rencana_perolehan = null;
+    //             $p->perkiraan_durasi = date_create($p->tanggal_mulai_terkontrak)->diff(date_create($p->tanggal_akhir_terkontrak))->days;
+    //             $p->periode = $request->periode;
+    //             unset($p->jenis_proyek, $p->unit_kerja, $p->kode_proyek, $p->stage, $p->tanggal_mulai_terkontrak, $p->tanggal_akhir_terkontrak);
 
-                return $p;
-            });
-            $data = [
-                "GetDetailProyekResult" => [
-                    "Success" => true,
-                    "Message" => null,
-                    "TotalData" => $proyeks->count(),
-                    "Data" => $proyeks,
-                ],
+    //             return $p;
+    //         });
+    //         $data = [
+    //             "GetDetailProyekResult" => [
+    //                 "Success" => true,
+    //                 "Message" => null,
+    //                 "TotalData" => $proyeks->count(),
+    //                 "Data" => $proyeks,
+    //             ],
 
-            ];
-            return response()->json($data);
-        }
-        return response()->json([
-            "status" => 401,
-            "msg" => "Tidak Terautentikasi"
-        ]);
-    })->middleware("userAuth");
+    //         ];
+    //         return response()->json($data);
+    //     }
+    //     return response()->json([
+    //         "status" => 401,
+    //         "msg" => "Tidak Terautentikasi"
+    //     ]);
+    // })->middleware("userAuth");
 
 
     // Begin - Detail Proyek yang ada forecast
@@ -152,7 +152,7 @@ Route::middleware(["web"])->group(function () {
 
             // $forecasts = Forecast::with(["Proyek"])->get(["*"])->unique("kode_proyek");
             // $forecasts = Forecast::where("periode_prognosa", '=', (int) $prognosa)->whereYear("created_at", "=", $tahun)->get();
-            $proyeks = HistoryForecast::join("proyeks", "proyeks.kode_proyek", "=", "history_forecast.kode_proyek")->where("unit_kerja", "=", $request->unitkerjaid)->get(["nama_proyek", "stage", "kode_proyek", "unit_kerja", "jenis_proyek", "tipe_proyek", "nilai_perolehan", "is_cancel"])->where("stage", "!=", 7)->where("is_cancel", "!=", true);
+            $proyeks = HistoryForecast::join("proyeks", "proyeks.kode_proyek", "=", "history_forecast.kode_proyek")->where("unit_kerja", "=", $request->unitkerjaid)->get(["nama_proyek", "stage", "proyeks.kode_proyek", "unit_kerja", "jenis_proyek", "tipe_proyek", "nilai_perolehan", "is_cancel"])->where("stage", "!=", 7)->where("is_cancel", "!=", true);
             
             $proyeks = $proyeks->map(function ($p) use ($periode) {
                 if($p->tipe_proyek == "R") {
@@ -177,11 +177,11 @@ Route::middleware(["web"])->group(function () {
                     }
                     $data_ok = collect();
                     for ($i = 1; $i <= 12; $i++) {
-                        $f = HistoryForecast::where("periode_prognosa", '=', $periode[1])->where("kode_proyek", '=', $p->kode_proyek)->where("month_forecast", "=", $i)->first();
-                        if (!empty($f) && $i == $f->month_forecast) {
+                        // $f = HistoryForecast::where("periode_prognosa", '=', $periode[1])->where("kode_proyek", '=', $p->kode_proyek)->where("month_forecast", "=", $i)->first();
+                        if (!empty($p) && $i == $p->month_forecast) {
                             $data_ok->push([
                                 "month" => $i,
-                                "total" => (int) $f->nilai_forecast
+                                "total" => (int) $p->nilai_forecast
                             ]);
                         } else {
                             $data_ok->push([
@@ -213,11 +213,11 @@ Route::middleware(["web"])->group(function () {
                     }
                     $data_ok = collect();
                     for ($i = 1; $i <= 12; $i++) {
-                        $f = HistoryForecast::where("periode_prognosa", '=', $periode[1])->where("kode_proyek", '=', $p->kode_proyek)->where("month_forecast", "=", $i)->first();
-                        if (!empty($f) && $i == $f->month_forecast) {
+                        // $f = HistoryForecast::where("periode_prognosa", '=', $periode[1])->where("kode_proyek", '=', $p->kode_proyek)->where("month_forecast", "=", $i)->first();
+                        if (!empty($p) && $i == $p->month_forecast) {
                             $data_ok->push([
                                 "month" => $i,
-                                "total" => (int) $f->nilai_forecast
+                                "total" => (int) $p->nilai_forecast
                             ]);
                         } else {
                             $data_ok->push([
