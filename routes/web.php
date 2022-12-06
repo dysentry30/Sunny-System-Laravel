5ccb856af8822deb8c8c8bd421d61b368dd501be
dysentry30 <bagassatrianurwinanto@gmail.com>
2022-12-07 02:47:00 +0700

    debug Industry Sector

--- routes/web.php
+++ routes/web.php
@@ -1079,7 +1079,7 @@ Route::group(['middleware' => ["userAuth", "admin"]], function () {
         // $provinsi = Provinsi::all();
         try {
             // $industrySector = collect(json_decode(Http::get("https://fioridev.wika.co.id/ywikasd002/industry-sector?sap-client=200")));
-            $industrySector = collect(json_decode(Http::withoutVerifying()->withOptions(["verify"=>false])->get("https://fioridev.wika.co.id/ywikasd002/industry-sector?sap-client=200")));
+            $industrySector = collect(json_decode(Http::withOptions(["verify"=>false])->get("https://fioridev.wika.co.id/ywikasd002/industry-sector?sap-client=200")));
             dd($industrySector);
             // $industrySector = collect([
             //     "devid" => "",
