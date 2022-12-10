6f2d52d42818fb74b4b60eb48d4b24d5519e4050
rahmadar-if <rahmadarifwork@gmail.com>
2022-12-10 13:41:51 +0700

    group

--- routes/web.php
+++ routes/web.php
@@ -1368,7 +1368,7 @@ Route::group(['middleware' => ["userAuth", "admin"]], function () {
         //     return $is_forecast_exist;
         // });
 
-        $history_forecasts = HistoryForecast::join("proyeks", "proyeks.kode_proyek", "=", "history_forecast.kode_proyek")->where("periode_prognosa", ((int) date('m')))->join("dops", "dops.dop", "=", "proyeks.dop")->join("unit_kerjas", "unit_kerjas.divcode", "=", "proyeks.unit_kerja");
+        $history_forecasts = HistoryForecast::join("proyeks", "proyeks.kode_proyek", "=", "history_forecast.kode_proyek")->join("dops", "dops.dop", "=", "proyeks.dop")->join("unit_kerjas", "unit_kerjas.divcode", "=", "proyeks.unit_kerja");
         $history_forecasts = $history_forecasts->get()->groupBy("unit_kerja");
         dd($history_forecasts);
         return view("/12_Autorisasi", compact("history_forecasts"));
