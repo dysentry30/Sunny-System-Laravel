a2dbeff1412f94fb16f0f58117f847e1fd47e783
rahmadar-if <rahmadarifwork@gmail.com>
2022-12-16 17:08:15 +0700

    datatables offline cdn

--- resources/views/12_Autorisasi.blade.php
+++ resources/views/12_Autorisasi.blade.php
@@ -365,13 +365,13 @@
 @section('js-script')
 
     <!--begin::Data Tables-->
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script> 
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script> 
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script> 
+    <script src="datatables/jquery.dataTables.min.js"></script>
+    <script src="datatables/dataTables.buttons.min.js"></script>
+    <script src="datatables/buttons.html5.min.js"></script>
+    <script src="datatables/buttons.colVis.min.js"></script>
+    <script src="datatables/jszip.min.js"></script>
+    <script src="datatables/pdfmake.min.js"></script>
+    <script src="datatables/vfs_fonts.js"></script>
     
     <script>
         $(document).ready(function() {
