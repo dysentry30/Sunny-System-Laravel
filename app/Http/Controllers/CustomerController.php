d217f5970d77f1e0db8766610266ccae30090492
rahmadar-if <rahmadarifwork@gmail.com>
2022-12-28 14:16:03 +0700

    cust

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -512,9 +512,9 @@ class CustomerController extends Controller
         $editCustomer->forbes_rank = $data["forbes_rank"];
         $editCustomer->lq_rank = $data["lq_rank"];
         $editCustomer->layer_segmentasi = $data["layer_segmentasi"];
-        $editCustomer->jenis_perusahaan = $data["jenis_perusahaan"];
-        $editCustomer->tax = $data["tax"];
-        $editCustomer->syarat_pembayaran = $data["syarat_pembayaran"];
+        // $editCustomer->jenis_perusahaan = $data["jenis_perusahaan"];
+        // $editCustomer->tax = $data["tax"];
+        // $editCustomer->syarat_pembayaran = $data["syarat_pembayaran"];
         $editCustomer->unique_code = $data["unique_code"];
 
         // $editCustomer->journey_company = $data["journey-company"];
