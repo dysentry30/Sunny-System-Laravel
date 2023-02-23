b5146f14d3bd9ec60a2b014d9daf03730ff52213
dysentry30 <bagassatrianurwinanto@gmail.com>
2023-02-23 16:10:49 +0700

    Update: - Matriks Approval Rekomendasi modify Jabatan to Pegawai

--- app/Models/MatriksApprovalRekomendasi.php
+++ app/Models/MatriksApprovalRekomendasi.php
@@ -11,10 +11,14 @@ class MatriksApprovalRekomendasi extends Model
     protected $table = "matriks_approval_rekomendasi";
     protected $primaryKey = "id_matriks_approval_rekomendasi";
 
-    public function Jabatan() {
-        return $this->hasOne(Jabatan::class, "kode_jabatan", "jabatan");
-    }
+    // public function Jabatan() {
+    //     return $this->hasOne(Jabatan::class, "kode_jabatan", "jabatan");
+    // }
     public function Divisi() {
         return $this->hasOne(Divisi::class, "id_divisi", "unit_kerja");
+    }
+
+    public function Pegawai() {
+        return $this->hasOne(Pegawai::class, "nip", "nama_pegawai");
     }
 }
