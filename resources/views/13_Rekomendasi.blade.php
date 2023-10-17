6b364ef453bef02df0d3e4f480c0893096c34c3b
Fathur Rohman <fathur.rohman2535@gmail.com>
2023-10-18 02:05:51 +0700

    update

--- resources/views/13_Rekomendasi.blade.php
+++ resources/views/13_Rekomendasi.blade.php
@@ -1022,7 +1022,7 @@
                                                                         <td>
                                                                             @foreach ($legalitasJasa as $key => $item)
                                                                             <div class="form-check" id="kriteria">
-                                                                                <input class="form-check-input" type="radio" name="is_legalitas" id="is_legalitas_{{ $key }}" onchange="setNilaiKriteria(this, '{{ (int)$item->bobot }}', '{{ $key }}')" value="{{ $key+1 }}">
+                                                                        <input class="form-check-input" type="radio" name="is_legalitas" id="is_legalitas_{{ $key }}" onchange="setNilaiKriteria(this, '{{ (int)$item->bobot }}', '{{ $key }}')" value="{{ $key+1 }}">
                                                                                 <label for="is_legalitas_{{ $key }}" class="form-check-label">
                                                                                     {!! nl2br($item->item) !!}
                                                                                 </label>
