fc96a1bbc95849a345cc88be5833d84234799b3b
Fathur Rohman <fathur.rohman2535@gmail.com>
2023-10-23 13:31:10 +0700

    update

--- resources/views/13_Rekomendasi.blade.php
+++ resources/views/13_Rekomendasi.blade.php
@@ -609,7 +609,7 @@
                                                                         })->first();
 
                                                                         if (empty($select_user)) {
-                                                                            $style = 'bg-secondary text-black';
+                                                                            $style = 'bg-secondary text-dark';
                                                                         }else{
                                                                             if ($select_user->status == "approved" && empty($select_user->alasan)) {
                                                                                 $style = 'bg-success';
