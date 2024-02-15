571d77df2feac5e76afd29d3df5a37a235513c4a
Fathur Rohman <fathur.rohman2535@gmail.com>
2024-02-15 13:52:07 +0700

    [BUGFIX] Penilaian Pengguna Jasa Models

---
+++ app/Models/PenilaianPenggunaJasa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PenilaianPenggunaJasa extends Model
+{
+    use HasFactory;
+    protected $table = "penilaian_risiko_pengguna_jasa";
+}
