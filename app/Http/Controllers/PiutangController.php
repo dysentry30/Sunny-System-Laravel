386e55a19fa3adcc9a94306b69bf709399a067f5
Fathur Rohman <fathur.rohman2535@gmail.com>
2024-05-20 15:42:53 +0700

    [UPDATE & BUGFIX] Dashboard dan Menu Piutang => 20 Mei 2024 - Fix Monitoring Proyek Dashboard - Tambah menu Piutang

--- app/Http/Controllers/PiutangController.php
+++ app/Http/Controllers/PiutangController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Piutang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class PiutangController extends Controller
 {
@@ -15,6 +17,87 @@ class PiutangController extends Controller
      */
     public function index(Request $request){
         $piutangs = Piutang::all();
-        return view("/Piutang/piutang", compact(["piutangs"]));
+        return view("/Piutang/piutang_new", compact(["piutangs"]));
+    }
+
+    public function save(Request $request, Piutang $piutang)
+    {
+        try {
+            $data = $request->all();
+
+            $rules = [
+                'nama_pelanggan' => "required",
+                'kode_proyek' => "required",
+                'status' => "required",
+            ];
+
+            $validation = validateInput($data, $rules);
+
+            if (!empty($validation)) {
+                Alert::html("Error", "Pastikan field <b>$validation</b> terisi!", "error");
+                return redirect()->back();
+            }
+
+            $piutang->customer_id = $data['nama_pelanggan'];
+            $piutang->kode_proyek = $data['kode_proyek'];
+            $piutang->kategori = $data['status'];
+            $piutang->created_by = Auth::user()->nip;
+            $piutang->updated_by = Auth::user()->nip;
+            $piutang->save();
+
+            Alert::success("Success", "Data Piutang Berhasil Dibuat!");
+            return redirect()->back();
+        } catch (\Exception $e) {
+            Alert::error("Error", $e->getMessage());
+            return redirect()->back();
+        }
+    }
+
+    public function edit(Request $request, Piutang $piutang)
+    {
+        try {
+            $data = $request->all();
+
+            $rules = [
+                // 'nama_pelanggan' => "required",
+                // 'kode_proyek' => "required",
+                'status' => "required",
+            ];
+
+            $validation = validateInput($data, $rules);
+
+            if (!empty($validation)) {
+                Alert::html("Error", "Pastikan field <b>$validation</b> terisi!", "error");
+                return redirect()->back();
+            }
+
+            if (empty($piutang)) {
+                Alert::html("Error", "Data Piutang Tidak Ditemukan <br> Hubungi Admin!", "error");
+                return redirect()->back();
+            }
+
+            $piutang->kategori = $data['status'];
+            $piutang->updated_by = Auth::user()->nip;
+            $piutang->save();
+
+            Alert::success("Success", "Data Piutang Berhasil Diubah!");
+            return redirect()->back();
+        } catch (\Exception $e) {
+            Alert::error("Error", $e->getMessage());
+            return redirect()->back();
+        }
+    }
+
+    public function delete(Request $request, Piutang $piutang)
+    {
+        try {
+            $piutang->delete();
+            Alert::success("Success", "Data Piutang Berhasil Dihapus!");
+            return redirect()->back();
+        } catch (\Exception $e) {
+            Alert::error("Error", $e->getMessage());
+            return redirect()->back();
+        }
+
     }
 }
