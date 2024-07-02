e38a86316aa6e6e8ed7582e645500699a5da3f48
Fathur Rohman <fathur.rohman2535@gmail.com>
2024-07-02 23:06:58 +0700

    [UPDATE] CREATE JOB GET PEGAWAI HCMS => 02 Juli 2024

--- routes/web.php
+++ routes/web.php
@@ -4247,26 +4247,29 @@ Route::group(['middleware' => ["userAuth", "admin"]], function () {
                     "verify" => false
                     // "debug" => $test_debug
                 ])->get("https://hcms.wika.co.id/apiwika/?method=get_pegawai&key=93hDk1L&client=crm&page=$i");
-                setLogging("api", "GET DATA PEGAWAI => ", [$data->json()]);
+                // setLogging("api", "GET DATA PEGAWAI => ", [$data->json()]);
                 $response = json_decode($data->body(), true);
                 if ($data->ok() && !empty($data->body()) && $response["status"] == 200) {
                     $pegawaiData = collect($data->json()["data"])->where('active', '!=', 0);
+                    dd($data->json());
                     if (!empty($pegawaiData)) {
                         $pegawaiData->each(function ($pegawai) {
                             $is_pegawai_exist = Pegawai::where('nip', $pegawai["nip"])->first();
-                            dd($is_pegawai_exist, $pegawai);
+                            // dd($is_pegawai_exist, $pegawai);
                             if (!empty($is_pegawai_exist)) {
                                 $is_pegawai_exist->nip = $pegawai["nip"] ?? null;
                                 $is_pegawai_exist->nama_pegawai = $pegawai["nm_peg"] ?? null;
                                 $is_pegawai_exist->handphone = $pegawai["telepon"] ?? null;
                                 $is_pegawai_exist->email = $pegawai["email"] ?? null;
                                 // $is_pegawai_exist->kode_jabatan = (int)$pegawai["kd_kantor"] ?? null;
-                                // $is_pegawai_exist->kode_jabatan_sap = (int)$pegawai["kd_jabatan"] ?? null;
-                                // $is_pegawai_exist->kode_fungsi_bidang_sap = (int)$pegawai["kd_fungsi_bidang"];
+                                $is_pegawai_exist->kode_jabatan_sap = (int)$pegawai["kd_jabatan"] ?? null;
+                                $is_pegawai_exist->kode_fungsi_bidang_sap = (int)$pegawai["kd_fungsi_bidang"];
                                 // $is_pegawai_exist->kode_fungsi_bidang = (int)$pegawai["kd_posisi"] ?? null;
-                                // $is_pegawai_exist->nama_fungsi_bidang = $pegawai["nm_fungsi_bidang"] ?? null;
-                                // $is_pegawai_exist->kode_kantor_sap = $pegawai["cmp_id"] ?? null;
-                                // $is_pegawai_exist->nama_kantor = $pegawai["jns_kantor"] ?? null;
+                                $is_pegawai_exist->nama_fungsi_bidang = $pegawai["nm_fungsi_bidang"] ?? null;
+                                $is_pegawai_exist->kode_kantor_sap = $pegawai["kd_kantor"] ?? null;
+                                $is_pegawai_exist->nama_kantor = $pegawai["nm_kantor"] ?? null;
+                                $is_pegawai_exist->kd_posisi = $pegawai["kd_posisi"] ?? null;
+                                $is_pegawai_exist->posisi = $pegawai["posisi"] ?? null;
                                 $is_pegawai_exist->save();
                             } else {
                                 $new_pegawai = new Pegawai();
@@ -4274,13 +4277,15 @@ Route::group(['middleware' => ["userAuth", "admin"]], function () {
                                 $new_pegawai->nama_pegawai = $pegawai["nm_peg"] ?? null;
                                 $new_pegawai->handphone = $pegawai["telepon"] ?? null;
                                 $new_pegawai->email = $pegawai["email"] ?? null;
-                                $new_pegawai->kode_jabatan = (int)$pegawai["kd_kantor"] ?? null;
+                                // $new_pegawai->kode_jabatan = (int)$pegawai["kd_kantor"] ?? null;
                                 $new_pegawai->kode_jabatan_sap = (int)$pegawai["kd_jabatan"] ?? null;
                                 $new_pegawai->kode_fungsi_bidang_sap = (int)$pegawai["kd_fungsi_bidang"];
-                                $new_pegawai->kode_fungsi_bidang = (int)$pegawai["kd_posisi"] ?? null;
+                                // $new_pegawai->kode_fungsi_bidang = (int)$pegawai["kd_posisi"] ?? null;
                                 $new_pegawai->nama_fungsi_bidang = $pegawai["nm_fungsi_bidang"] ?? null;
-                                $new_pegawai->kode_kantor_sap = $pegawai["cmp_id"] ?? null;
-                                $new_pegawai->nama_kantor = $pegawai["jns_kantor"] ?? null;
+                                $new_pegawai->kode_kantor_sap = $pegawai["kd_kantor"] ?? null;
+                                $new_pegawai->nama_kantor = $pegawai["nm_kantor"] ?? null;
+                                $new_pegawai->kd_posisi = $pegawai["kd_posisi"] ?? null;
+                                $new_pegawai->posisi = $pegawai["posisi"] ?? null;
                                 $new_pegawai->save();
                             }
                         });
