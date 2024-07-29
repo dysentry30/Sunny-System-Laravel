299586724df4060ad510877acd9e8925b7def90c
Fathur Rohman <fathur.rohman2535@gmail.com>
2024-07-29 15:35:32 +0700

    [UPDATE] SCHEDULE FORECAST DAN CLAIM => 29 Juli 2024

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +17,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('schedule:forecast')->monthly()->runInBackground()->onSuccess(function () {
+            Artisan::call("schedule:contract");
+        });
     }
 
     /**
