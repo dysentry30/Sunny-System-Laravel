4c5d503037fdcb43cff30846ecbfaf051a6ebdbc
Fathur Rohman <fathur.rohman2535@gmail.com>
2024-10-03 14:54:47 +0700

    [UPDATE] MENU MANAGEMENT - FIXING DOUBLE USE LIBRARY GATE => 03 Oktober 2024

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Models\Divisi;
-use Illuminate\Support\Facades\Gate;
 use App\Models\MatriksApprovalChangeManagement;
 use App\Models\UserMenuManagement;
 use Illuminate\Support\Facades\Gate;
